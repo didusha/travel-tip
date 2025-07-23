d26b81c17f4df6ae3bfe937d2ace1e2e77368c6b
Adi <dihash@gmail.com>
2025-07-23 16:43:46 +0300

    Add sort by createAt

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -54,8 +54,9 @@ function query() {
                 locs.sort((p1, p2) => (p1.rate - p2.rate) * gSortBy.rate)
             } else if (gSortBy.name !== undefined) {
                 locs.sort((p1, p2) => p1.name.localeCompare(p2.name) * gSortBy.name)
-            } else if(locs.createdAt !== undefined)
-
+            } else if(gSortBy.createdAt !== undefined){
+                locs.sort((ct1, ct2) => (ct2.createdAt-ct1.createdAt) * gSortBy.createdAt)
+            }
             return locs
         })
 }
@@ -104,7 +105,7 @@ function getLocCountByRateMap() {
 }
 
 function setSortBy(sortBy = {}) {
-    gSortBy = sortBy
+    gSortBy = sortBy    
 }
 
 function _createLocs() {
